38de4f89c60a82eed10d292ab26319ff46995efe
ran2king06 <ran2king06@gmail.com>
2024-07-26 00:27:44 +0800

    feat: add preload source

--- src/imageSources.js
+++ src/imageSources.js
@@ -4,6 +4,65 @@ import BtnIKnow from './assets/img/btn/btn-iknow.png';
 import IconCamera from './assets/img/icon/icon-camera.png';
 import IconMap from './assets/img/icon/icon-map.png';
 import IconPrize from './assets/img/icon/icon-prize.png';
+import MapA from './assets/img/map/a.png';
+import MapB from './assets/img/map/b.png';
+import MapC from './assets/img/map/c.png';
+import MapD from './assets/img/map/d.png';
+import MapE from './assets/img/map/e.png';
+import KMMap from './assets/img/map/KMap.png';
+import ResultImg1 from './assets/img/result/1.png';
+import ResultImg2 from './assets/img/result/2.png';
+import ResultImg3 from './assets/img/result/3.png';
+import ResultImg4 from './assets/img/result/4.png';
+import ResultImg5 from './assets/img/result/5.png';
+import TourImageA1 from './assets/img/tour/a/1.png';
+import TourImageA2 from './assets/img/tour/a/2.png';
+import TourImageA3 from './assets/img/tour/a/3.png';
+import TourImageA4 from './assets/img/tour/a/4.png';
+import TourImageA5 from './assets/img/tour/a/5.png';
+import TourImageB1 from './assets/img/tour/b/1.png';
+import TourImageB2 from './assets/img/tour/b/2.png';
+import TourImageB3 from './assets/img/tour/b/3.png';
+import TourImageB4 from './assets/img/tour/b/4.png';
+import TourImageB5 from './assets/img/tour/b/5.png';
+import TourImageC1 from './assets/img/tour/c/1.png';
+import TourImageC101 from './assets/img/tour/c/10/1.png';
+import TourImageC1010 from './assets/img/tour/c/10/10.png';
+import TourImageC102 from './assets/img/tour/c/10/2.png';
+import TourImageC103 from './assets/img/tour/c/10/3.png';
+import TourImageC104 from './assets/img/tour/c/10/4.png';
+import TourImageC105 from './assets/img/tour/c/10/5.png';
+import TourImageC106 from './assets/img/tour/c/10/6.png';
+import TourImageC107 from './assets/img/tour/c/10/7.png';
+import TourImageC108 from './assets/img/tour/c/10/8.png';
+import TourImageC109 from './assets/img/tour/c/10/9.png';
+import TourImageC2 from './assets/img/tour/c/2.png';
+import TourImageC71 from './assets/img/tour/c/7/1.png';
+import TourImageC72 from './assets/img/tour/c/7/2.png';
+import TourImageC73 from './assets/img/tour/c/7/3.png';
+import TourImageC74 from './assets/img/tour/c/7/4.png';
+import TourImageC75 from './assets/img/tour/c/7/5.png';
+import TourImageC76 from './assets/img/tour/c/7/6.png';
+import TourImageC81 from './assets/img/tour/c/8/1.png';
+import TourImageC82 from './assets/img/tour/c/8/2.png';
+import TourImageC83 from './assets/img/tour/c/8/3.png';
+import TourImageC84 from './assets/img/tour/c/8/4.png';
+import TourImageC85 from './assets/img/tour/c/8/5.png';
+import TourImageC91 from './assets/img/tour/c/9/1.png';
+import TourImageC92 from './assets/img/tour/c/9/2.png';
+import TourImageC93 from './assets/img/tour/c/9/3.png';
+import TourImageC94 from './assets/img/tour/c/9/4.png';
+import TourImageC95 from './assets/img/tour/c/9/5.png';
+import TourImageC96 from './assets/img/tour/c/9/6.png';
+import TourImageD1 from './assets/img/tour/d/1.png';
+import TourImageD2 from './assets/img/tour/d/2.png';
+import TourImageD3 from './assets/img/tour/d/3.png';
+import TourImageE1 from './assets/img/tour/e/1.png';
+import TourImageE2 from './assets/img/tour/e/2.png';
+import TourImageE3 from './assets/img/tour/e/3.png';
+import TourImageE4 from './assets/img/tour/e/4.png';
+import TourImageE5 from './assets/img/tour/e/5.png';
+import TourImageE6 from './assets/img/tour/e/6.png';
 import Bear1 from './pages/scene_start/img/bear1.png';
 import Bear2 from './pages/scene_start/img/bear2.png';
 import Bear3 from './pages/scene_start/img/bear3.png';
@@ -25,6 +84,7 @@ import Location3 from './pages/scene_start/img/location/3.png';
 import Location4 from './pages/scene_start/img/location/4.png';
 import Location5 from './pages/scene_start/img/location/5.png';
 import StartTitle from './pages/scene_start/img/title.png';
+
 
 // 匯入所有圖片
 // 將所有圖片放入陣列
@@ -54,6 +114,65 @@ const ImageSources = [
   Location4,
   Location5,
   StartTitle,
+  MapA,
+  MapB,
+  MapC,
+  MapD,
+  MapE,
+  KMMap,
+  ResultImg1,
+  ResultImg2,
+  ResultImg3,
+  ResultImg4,
+  ResultImg5,
+  TourImageA1,
+  TourImageA2,
+  TourImageA3,
+  TourImageA4,
+  TourImageA5,
+  TourImageB1,
+  TourImageB2,
+  TourImageB3,
+  TourImageB4,
+  TourImageB5,
+  TourImageC1,
+  TourImageC2,
+  TourImageC71,
+  TourImageC72,
+  TourImageC73,
+  TourImageC74,
+  TourImageC75,
+  TourImageC76,
+  TourImageC81,
+  TourImageC82,
+  TourImageC83,
+  TourImageC84,
+  TourImageC85,
+  TourImageC91,
+  TourImageC92,
+  TourImageC93,
+  TourImageC94,
+  TourImageC95,
+  TourImageC96,
+  TourImageC101,
+  TourImageC102,
+  TourImageC103,
+  TourImageC104,
+  TourImageC105,
+  TourImageC106,
+  TourImageC107,
+  TourImageC108,
+  TourImageC109,
+  TourImageC1010,
+  TourImageD1,
+  TourImageD2,
+  TourImageD3,
+  TourImageE1,
+  TourImageE2,
+  TourImageE3,
+  TourImageE4,
+  TourImageE5,
+  TourImageE6,
 
 ]
 
