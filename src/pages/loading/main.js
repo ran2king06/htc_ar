462c5f78de6261729390d71929990e365aabd258
ran2king06 <ran2king06@gmail.com>
2024-12-17 21:10:39 +0800

    feat: update

--- src/pages/loading/main.js
+++ src/pages/loading/main.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import LoadingImg from './img/loading.png';
+import LoadingImg01 from './img/loading.png';
+import LoadingImg02 from './img/starting.png';
 
 const Loading = () => {
+  const [loadingPercent, setLoadingPercent] = React.useState(0);
+  const [loadingImg, setLoadingImg] = React.useState(LoadingImg01);
+
+  useEffect(() => {
+    // 10秒，然後第10秒停留在80%，不再增加
+    let count = 0;
+    const timer = setInterval(() => {
+      count += 1;
+      setLoadingPercent(count);
+
+      if (count === 50) {
+        // clearInterval(timer);
+        setLoadingImg(LoadingImg02);
+
+        if (count === 100) {
+          clearInterval(timer);
+        }
+      }
+    }, 100);
+  }, []);
+
   return (
     <div className="loading-bg">
-      <img src={LoadingImg} alt="Loading" />
+      <img src={loadingImg} alt="Loading" />
+
+      <div class="loading-bar">
+        <span style={{ width: `${loadingPercent}%` }}></span>
+      </div>
     </div>
   );
 }
